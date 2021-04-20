9238d0e8d571e99460ba96c3e95750f9530b2b97
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2021-04-20 23:30:37 +0200

    feat: implement nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,276 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+  useLocation,
+  useParams,
+  useRouteMatch
+} from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+const navigation = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'About', href: '/about', current: false },
+  { name: 'Topics', href: '/topics', current: false }
+]
 
+function classNames(...classNameNamees: string[]) {
+  return classNameNamees.filter(Boolean).join(' ')
+}
+
+const usePathname = () => {
+  const location = useLocation()
+  return location.pathname
+}
+
+const useTitle = () => {
+  const match = useRouteMatch()
+  console.log('match path', match.path)
+  return navigation.find((item) => item.href === match.path)?.name
+}
+
+const isCurrent = (item: string) => {
+  const match = useLocation().pathname
+  if (item === '/' && match === '/') return true
+  if (item === '/' && match !== '/') return false
+  return match.startsWith(item)
+}
+
+const Header = () => {
   return (
-    <div className="border border-gray-50 rounded-xl p-20 shadow-xl">
-      <header>
-        <div className="flex justify-center">
-          <img src={logo} className="h-32 w-32 animate-spin-slow" alt="logo" />
+    <header>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold leading-tight text-gray-900">
+          <div>{useTitle()}</div>
+        </h1>
+      </div>
+    </header>
+  )
+}
+
+const Menu = () => {
+  return (
+    <nav className="bg-white border-b border-gray-200">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex">
+            <div className="flex-shrink-0 flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="block h-8 w-auto text-cerise-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
+                />
+              </svg>
+            </div>
+            <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+              {navigation.map((item) => (
+                <Link
+                  key={item.name}
+                  to={item.href}
+                  className={classNames(
+                    isCurrent(item.href)
+                      ? 'border-cerise-500 text-gray-900'
+                      : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                    'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          <div className="-mr-2 flex items-center sm:hidden">
+            <button
+              type="button"
+              className="bg-white inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cerise-500"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+
+              <svg
+                className="block h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+
+              <svg
+                className="hidden h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-        <p className="text-2xl pb-3">Hello Vite + React + tailwindcss!</p>
-        <p>
-          <button
-            className="bg-purple-400 pl-2 pr-2 pt-1 pb-1 rounded text-sm text-purple-100"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            count is: {count}
-          </button>
-        </p>
-        <p className="pb-3 pt-3">
-          Edit{' '}
-          <code className="border border-1 pl-1 pr-1 pb-0.5 pt-0.5 rounded border-purple-400 font-mono text-sm bg-purple-100 text-purple-900">
-            src/App.tsx
-          </code>{' '}
-          and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="text-purple-400 underline"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="text-purple-400 underline"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-          {' | '}
-          <a
-            className="text-purple-400 underline"
-            href="https://tailwindcss.com/docs"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            tailwindcss Docs
-          </a>
-        </p>
-      </header>
+      </div>
+
+      <div className="sm:hidden" id="mobile-menu">
+        <div className="pt-2 pb-3 space-y-1">
+          {navigation.map((item) => (
+            <Link
+              key={item.name}
+              to={item.href}
+              className={classNames(
+                isCurrent(item.href)
+                  ? 'bg-cerise-50 border-cerise-500 text-cerise-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                  : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+              )}
+              aria-current={item.current ? 'page' : undefined}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Menu></Menu>
+      <div className="py-10">
+        <Switch>
+          <Route path="/about">
+            <Header />
+          </Route>
+          <Route path="/topics">
+            <Header />
+          </Route>
+          <Route path="/">
+            <Header />
+          </Route>
+        </Switch>
+
+        <main>
+          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div className="px-4 py-8 sm:px-0">
+              <div className="bg-gray-100 rounded-lg h-96 p-10">
+                <Switch>
+                  <Route path="/about">
+                    <About />
+                  </Route>
+                  <Route path="/topics">
+                    <Topics />
+                  </Route>
+                  <Route path="/">
+                    <Home />
+                  </Route>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </Router>
+  )
+}
+
+function Home() {
+  return <div>Home content</div>
+}
+
+function About() {
+  return <div>About content</div>
+}
+
+function Topics() {
+  const match = useRouteMatch()
+  return (
+    <div>
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3 className="text-xl">Please select a topic.</h3>
+        </Route>
+      </Switch>
+      <div>
+        <ul>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 inline-block text-cerise-600"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+              <path
+                fillRule="evenodd"
+                d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <Link to={`${match.url}/components`} className="text-cerise-400">
+              Components
+            </Link>
+          </li>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5 inline-block text-cerise-600"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+              <path
+                fillRule="evenodd"
+                d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <Link to={`${match.url}/props-v-state`} className="text-cerise-400">
+              Props v. State
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
 
-export default App
+function Topic() {
+  const { topicId } = useParams() as { topicId: string }
+  return <h3 className="text-xl">Requested topic ID: {topicId}</h3>
+}
